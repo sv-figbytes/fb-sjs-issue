3e444ffebb1558ce98622e72f7622b65943becdc
sviazkov <sviazkov@figbytes.com>
2023-07-24 11:54:10 -0400

    init

--- apps/fb-r/src/app/app.module.ts
+++ apps/fb-r/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { SurveyModule } from 'survey-angular-ui'
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NxWelcomeComponent ],
+  imports: [BrowserModule, SurveyModule],
   providers: [],
   bootstrap: [AppComponent],
 })
